fe202fe7c2b580a6174cc6121dff4b26f143e53e
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2023-03-14 13:38:42 +0100

    Changed names in edit page

---
+++ Pizzaplace/Pizzaplace/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Pizzaplace.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Gutentag()
+        {
+            return "Hello";
+        }
+    }
+}
